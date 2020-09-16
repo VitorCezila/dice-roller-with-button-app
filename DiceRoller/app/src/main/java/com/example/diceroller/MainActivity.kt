192f4d5c50fe3dc63b5fa96d506a0bb01c350ec5
VitorCezila <vcezila@gmail.com>
2020-09-16 14:02:31 -0300

    Added one more dice to roll

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -32,8 +32,12 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
+        val dice2 = Dice(6)
+        val diceRoll2 = dice2.roll()
+
         // Find the ImageView in the layout
         val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
 
         // Determine which drawable resource ID to use based on the dice roll (SEE THIS CLASS AGAIN)
         val drawableResource = when (diceRoll) {
@@ -45,8 +49,19 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
+        // Determine which drawable resource ID to use based on the 2 dice roll
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
         // Update the imageView with the correct drawable resource ID
         diceImage.setImageResource(drawableResource)
+        diceImage2.setImageResource(drawableResource2)
 
         // Update the content description
         diceImage.contentDescription = diceRoll.toString()
